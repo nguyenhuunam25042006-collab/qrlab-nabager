0186e21f30a85e72f653d7eadda82180f6bc646c
nguyenhuunam25042006-collab <nguyenhuunam25042006@gmail.com>
2026-03-17 09:26:12 +0700

    Update devicies.js

--- devicies.js
+++ devicies.js
@@ -1,7 +1,18 @@
-const devices = {
+const devices={
 
-TB001:{name:"TB001",status:"free"},
-TB002:{name:"TB002",status:"using"},
-TB003:{name:"TB003",status:"broken"}
+"TB001":{
+name:"Máy tính Lab 1",
+status:"free"
+},
+
+"TB002":{
+name:"Máy tính Lab 2",
+status:"using"
+},
+
+"TB003":{
+name:"Máy chiếu",
+status:"broken"
+}
 
 };
