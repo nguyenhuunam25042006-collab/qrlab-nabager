6b5b17ae96585f04c34257a527c27b8e611e7083
nguyenhuunam25042006-collab <nguyenhuunam25042006@gmail.com>
2026-03-17 07:30:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
-const devices = [
- {id:"PC01", status:"free"},
- {id:"PC02", status:"busy"},
- {id:"PC03", status:"free"},
- {id:"PC04", status:"broken"},
- {id:"PC05", status:"free"}
-];
+function onScanSuccess(qrCodeMessage) {
+
+let device = devices.find(d => d.id === qrCodeMessage);
+
+if(!device){
+document.getElementById("result").innerHTML =
+"❌ Không tìm thấy máy";
+return;
+}
+
+if(device.status=="busy"){
+document.getElementById("result").innerHTML =
+"⚠️ Máy đang được sử dụng";
+}
+
+else if(device.status=="broken"){
+document.getElementById("result").innerHTML =
+"🔧 Máy đang hỏng";
+}
+
+else{
+document.getElementById("result").innerHTML =
+"✅ Máy sẵn sàng sử dụng";
+}
+
+}
+
+let scanner = new Html5QrcodeScanner(
+"reader",
+{fps:10, qrbox:250}
+);
+
+scanner.render(onScanSuccess);
+
+function reportBroken(){
+
+window.open("LINK_GOOGLE_FORM_BAO_HONG");
+
+}
+
+function bookMachine(){
+
+window.open("LINK_GOOGLE_FORM_DAT_LICH");
+
+}
