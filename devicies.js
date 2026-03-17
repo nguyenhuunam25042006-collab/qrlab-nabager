10e3f13dd9a4e7ae8f0d187106d096637440ab47
nguyenhuunam25042006-collab <nguyenhuunam25042006@gmail.com>
2026-03-17 08:01:05 +0700

    Update devicies.js

--- devicies.js
+++ devicies.js
@@ -1,7 +1,9 @@
 const devices = [
- {id:"PC01", status:"free"},
- {id:"PC02", status:"busy"},
- {id:"PC03", status:"free"},
- {id:"PC04", status:"broken"},
- {id:"PC05", status:"free"}
+
+{ id: "TB001", status: "free" },
+
+{ id: "TB002", status: "busy" },
+
+{ id: "TB003", status: "broken" }
+
 ];
