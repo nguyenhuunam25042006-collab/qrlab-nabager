cc14a1a8a9238b46f02585272259e0119e4fc64a
nguyenhuunam25042006-collab <nguyenhuunam25042006@gmail.com>
2026-03-18 14:03:44 +0700

    Update devicies.js

--- devicies.js
+++ devicies.js
@@ -1,18 +1,14 @@
-const devices={
+let devices = JSON.parse(localStorage.getItem("devices")) || {
 
-"TB001":{
-name:"Máy tính Lab 1",
-status:"free"
-},
-
-"TB002":{
-name:"Máy tính Lab 2",
-status:"using"
-},
-
-"TB003":{
-name:"Máy chiếu",
-status:"broken"
-}
+TB001:{name:"Tủ sấy",status:"free"},
+TB002:{name:"Thiết bị thí nghiệm hằn lún vết bánh xe",status:"free"},
+TB003:{name:"Thiết bị thí nghiệm Marshall tự động",status:"free"},
+TB004:{name:"Thiết bị đầm bê tông nhựa",status:"free"},
+TB005:{name:"Thiết bị xác định hàm lượng parafin trong nhựa",status:"free"},
+TB006:{name:"Thiết bị xác định độ kéo dài của nhựa",status:"free"},
+TB007:{name:"Thiết bị xác định độ nhớt Brookfield",status:"free"},
+TB008:{name:"Thiết bị xác định độ tổn thất khối lượng của nhựa đường",status:"free"},
+TB009:{name:"Máy cắt mẫu bê tông xi măng",status:"free"},
+TB010:{name:"Thiết bị bảo dưỡng mẫu bê tông xi măng",status:"free"}
 
 };
